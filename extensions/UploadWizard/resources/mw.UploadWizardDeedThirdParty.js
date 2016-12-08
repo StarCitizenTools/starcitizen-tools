a4c63bd55f2019e1406052b9cd773022973bbf12
Facerafter <bouman0707@gmail.com>
2016-12-08 23:59:55 +0100

    Added source validation and date is no longer required

--- extensions/UploadWizard/resources/mw.UploadWizardDeedThirdParty.js
+++ extensions/UploadWizard/resources/mw.UploadWizardDeedThirdParty.js
@@ -51,7 +51,9 @@
 				errors = [],
 				minLength = config.minSourceLength,
 				maxLength = config.maxSourceLength,
-				text = this.getValue().trim();
+				text = this.getValue().trim(),
+				template = new RegExp("{{.*}}"),
+				re_weburl = new RegExp("^" + "(?:(?:https?|ftp)://)" + "(?:\\S+(?::\\S*)?@)?" + "(?:" + "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +"(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" + "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" + "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" + "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" + "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" + "|" + "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" + "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" + "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" + "\\.?" + ")" + "(?::\\d{2,5})?" + "(?:[/?#]\\S*)?" + "$", "i");
 
 			if ( text === '' ) {
 				errors.push( mw.message( 'mwe-upwiz-error-blank' ) );
@@ -59,6 +61,8 @@
 				errors.push( mw.message( 'mwe-upwiz-error-too-short', minLength ) );
 			} else if ( text.length > maxLength ) {
 				errors.push( mw.message( 'mwe-upwiz-error-too-long', maxLength ) );
+			} else if (!re_weburl.test(text) || !template.test(text)) {
+				errors.push( mw.message( 'mwe-upwiz-error-not-url-or-template' ) );
 			}
 
 			return $.Deferred().resolve( errors ).promise();
