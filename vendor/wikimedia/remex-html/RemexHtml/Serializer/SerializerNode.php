1ce823c480e52a6059458454ee57564506e3c889
Facerafter <bouman0707@gmail.com>
2018-07-04 10:26:32 +0200

    Updating Vendor files...
    Apparently the vendor folder was being ignored.

---
+++ vendor/wikimedia/remex-html/RemexHtml/Serializer/SerializerNode.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace RemexHtml\Serializer;
+
+use RemexHtml\PropGuard;
+
+class SerializerNode {
+	use PropGuard;
+
+	public $id;
+	public $parentId;
+	public $namespace;
+	public $name;
+	public $attrs;
+	public $void;
+	public $children = [];
+
+	/**
+	 * Arbitrary user data can be placed here.
+	 */
+	public $snData;
+
+	public function __construct( $id, $parentId, $namespace, $name, $attrs, $void ) {
+		$this->id = $id;
+		$this->parentId = $parentId;
+		$this->namespace = $namespace;
+		$this->name = $name;
+		$this->attrs = $attrs;
+		$this->void = $void;
+	}
+}
