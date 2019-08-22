bdf8e0bd30f4db2ff52c10d995dccc59e7bd4683
alistair3149 <alistair31494322@gmail.com>
2019-08-22 03:22:39 -0400

    Disabled NativeSVGHandler due to security issues, changed External Data load extension method

--- LocalSettings.php
+++ LocalSettings.php
@@ -197,11 +197,11 @@ wfLoadExtension( 'PageImages' );
 wfLoadExtension( 'VisualEditor' );
 wfLoadExtension( 'Scribunto' );
 wfLoadExtension( 'Renameuser' );
+wfLoadExtension( 'ExternalData' );
 require_once "$IP/extensions/Translate/Translate.php";
 require_once "$IP/extensions/googleAnalytics/googleAnalytics.php";
 require_once "$IP/extensions/EventLogging/EventLogging.php";
-require_once "$IP/extensions/ExternalData/ExternalData.php";
-require_once "$IP/extensions/NativeSvgHandler/NativeSvgHandler.php";
+#require_once "$IP/extensions/NativeSvgHandler/NativeSvgHandler.php";
 #require_once "$IP/extensions/Lazyload/Lazyload.php";
 #require_once "$IP/extensions/Antispam/Antispam.php";
 
