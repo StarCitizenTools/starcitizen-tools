fd89827c42e953dcfe4fc3fdd14204cf53c89122
alistair3149 <alistair31494322@gmail.com>
2019-12-13 17:42:32 -0500

    Reverted nsvg to older version

---
+++ extensions/NativeSvgHandler/NativeSvgHandler.i18n.php
@@ -0,0 +1,61 @@
+<?php
+/*
+    Internationalization file for NativeSvgHandler extension
+
+    Copyright 2012 Povilas Kanapickas <povilas@radix.lt>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$messages = array();
+
+/** English
+* @author p12tic
+*/
+$messages['en'] = array(
+    'nativesvghandler' => 'Native SVG Handler',
+    'nativesvghandler_desc' => 'Serves SVG images directly to clients'
+);
+
+/** German (Deutsch)
+* @author Kghbln
+*/
+$messages['de'] = array(
+    'nativesvghandler' => 'Native SVG Handler',
+    'nativesvghandler_desc' => 'Ermöglicht die direkte Darstellung von SVG-Dateien im Client'
+);
+
+/** French (français)
+* @author ilai-deutel
+*/
+$messages['fr'] = array(
+    'nativesvghandler' => 'Native SVG Handler',
+    'nativesvghandler_desc' => 'Permet l\'affichage des fichiers SVG directement par le navigateur'
+);
+
+/** Korean (한국어)
+* @author 아라
+*/
+$messages['ko'] = array(
+    'nativesvghandler' => '네이티브 SVG 핸들러',
+    'nativesvghandler_desc' => '클라이언트에 직접 SVG 그림을 제공합니다'
+);
+
+/** Swedish (svenska)
+* @author Rotsee
+*/
+$messages['sv'] = array(
+    'nativesvghandler' => 'Inbäddad SVG-visning',
+    'nativesvghandler_desc' => 'Visar SVG-bilder direkt i webbläsaren'
+);
