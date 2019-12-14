b6ba9a8ade5f13d17efcf24868a37240cf3252fe
alistair3149 <alistair31494322@gmail.com>
2019-12-13 22:35:22 -0500

    Added link wrapper to nsvg

--- extensions/NativeSvgHandler/includes/SvgImage.php
+++ extensions/NativeSvgHandler/includes/SvgImage.php
@@ -20,6 +20,34 @@ class SvgImage extends MediaTransformOutput {
 
         $alt = empty( $options['alt'] ) ? '' : $options['alt'];
 
+        $query = empty( $options['desc-query'] ) ? '' : $options['desc-query'];
+        if ( !empty( $options['custom-url-link'] ) ) {
+            $linkAttribs = array( 'href' => $options['custom-url-link'] );
+            if ( !empty( $options['title'] ) ) {
+                $linkAttribs['title'] = $options['title'];
+            }
+            if ( !empty( $options['custom-target-link'] ) ) {
+                $linkAttribs['target'] = $options['custom-target-link'];
+            } elseif ( !empty( $options['parser-extlink-target'] ) ) {
+                $linkAttribs['target'] = $options['parser-extlink-target'];
+            }
+            if ( !empty( $options['parser-extlink-rel'] ) ) {
+                $linkAttribs['rel'] = $options['parser-extlink-rel'];
+            }
+        } elseif ( !empty( $options['custom-title-link'] ) ) {
+            $title = $options['custom-title-link'];
+            $linkAttribs = array(
+                'href' => $title->getLinkURL(),
+                'title' => empty( $options['title'] ) ? $title->getFullText() : $options['title']
+            );
+        } elseif ( !empty( $options['desc-link'] ) ) {
+            $linkAttribs = $this->getDescLinkAttribs( empty( $options['title'] ) ? null : $options['title'], $query );
+        } elseif ( !empty( $options['file-link'] ) ) {
+            $linkAttribs = array( 'href' => $this->file->getURL() );
+        } else {
+            $linkAttribs = false;
+        }
+        
         $attribs = array(
             'alt' => $alt,
             'src' => $this->url,
@@ -32,6 +60,6 @@ class SvgImage extends MediaTransformOutput {
         if ( !empty( $options['img-class'] ) ) {
             $attribs['class'] = $options['img-class'];
         }
-        return Xml::element('img', $attribs);
+        return $this->linkWrap( $linkAttribs, Xml::element( 'img', $attribs ) );
     }
 }
