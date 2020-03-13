e12b5eb7e2375f589cba00c744bebef5fdf081eb
alistair3149 <alistair3149@users.noreply.github.com>
2020-03-13 16:39:51 -0400

    Updated if condition for canonical URL override

--- LocalSettings.php
+++ LocalSettings.php
@@ -686,7 +686,7 @@ $wgHooks['GetLocalURL'][] = function ( &$title, &$url, $query ) {
 
 # Override canonical URL setting for main page
 $wgHooks['BeforePageDisplay'][] = function ( OutputPage $out, Skin $skin ) {
-	if ( $out->getPageTitle() == $wgMainPageTitle  ) {
+	if ( $out->getPageTitle() == 'Star_Citizen_Wiki'  ) {
 		$out->addLink( [
 			'rel' => 'canonical',
 			'href' => $wgServer,
